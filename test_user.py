cdf1f5daad4e36b39f1eedd5a4c9652d8c66a608
easthermutheumwengei <easther.mwengei@student.moringaschool.com>
2021-08-02 12:46:30 +0300

    create testunit

--- test_user.py
+++ test_user.py
@@ -19,7 +19,7 @@ Args:
         """
 
         self.account1 = Credentials(
-            'Twitter', 'esthermutheu99@gmail.com', '_esther', 'p@$$w0rD', 'haddasah')
+            'Twitter', 'esthermutheu99@gmail.com', '_dylandaxy', 'p@$$w0rD', 'dylan')
         self.account2 = Credentials(
             'Gmail', 'esthermutheu99@gmail.com', 'rass', 'l0v3_L33', 'haddasah')
 
@@ -27,3 +27,12 @@ Args:
             """tearDown method that does clean up after each test case has run.
         """
         Credentials.accounts_list = []
+
+        def test_init(self):
+            """test_init test case to test if the object is initialized properly
+        """
+        self.assertEqual(self.account1.account_name, 'Twitter')
+        self.assertEqual(self.account1.email, 'esthermutheu99@gmail.com')
+        self.assertEqual(self.account1.username, '_dylandaxy')
+        self.assertEqual(self.account1.password, 'p@$$w0rD')
+        self.assertEqual(self.account1.user, 'dylan')
