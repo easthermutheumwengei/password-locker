372466f91efa09bab872156f30149651de5b1a3f
easthermutheumwengei <easther.mwengei@student.moringaschool.com>
2021-08-02 13:19:04 +0300

    create a function for new contact

--- run.py
+++ run.py
@@ -4,3 +4,8 @@ from user import Credentials, User
 
 
 def create_account(name, emil, uname, pw, user):
+    def create_account(name, email, uname, pw, user):
+        """Function to create a new contact.
+    """
+    new_credential = Credentials(name, email, uname, pw, user)
+    return new_credential
